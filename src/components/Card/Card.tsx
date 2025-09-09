856d7b8b80ba1c1a5dcf1dc1fc16e7bec953095e
marta2611379 <marta2611379@gmail.com>
2025-09-09 11:26:49 +0300

    fix

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import { CartProvider, useCart } from "../../context/CartContext";
+import { useCart } from "../../context/CartContext";
 import type { CardSetting } from "../../types/type";
 import Button from "../Button/Button";
 
@@ -27,58 +27,55 @@ function Card({ product }: { product: CardSetting }) {
   }
 
   return (
-    <CartProvider>
-      <div className="bg-white rounded-2xl shadow-md p-4 flex flex-col items-center hover:shadow-lg transition">
-        <img
-          src={product.image}
-          alt={product.title}
-          className="w-56 h-56 object-cover rounded-xl"
-        />
-        <div className="flex flex-col items-start mt-3 w-full">
-          <div className="flex items-center justify-between w-full mb-1">
-            <p className="text-lg font-semibold">{product.price}₴</p>
-            {counterCartItems(product.id) > 0 ? (
-              <div className="flex items-center justify-between gap-3">
-                <Button
-                  className="size-8"
-                  onClick={() =>
-                    counterCartItems(product.id) > 1
-                      ? decrementItem(product.id)
-                      : removeFromCart(product.id)
-                  }
-                >
-                  -
-                </Button>
-                <span className="text-lg font-semibold">
-                  {counterCartItems(product.id)}
-                </span>
-
-                <Button
-                  className="size-8"
-                  onClick={() => incrementItem(product.id)}
-                >
-                  +
-                </Button>
-              </div>
-            ) : (
-              <Button className="h-8" onClick={() => addToCart(product)}>
-                У кошик
+    <div className="bg-white rounded-2xl shadow-md p-4 flex flex-col items-center hover:shadow-lg transition">
+      <img
+        src={product.image}
+        alt={product.title}
+        className="w-56 h-56 object-cover rounded-xl"
+      />
+      <div className="flex flex-col items-start mt-3 w-full">
+        <div className="flex items-center justify-between w-full mb-1">
+          <p className="text-lg font-semibold">{product.price}₴</p>
+          {counterCartItems(product.id) > 0 ? (
+            <div className="flex items-center justify-between gap-3">
+              <Button
+                className="size-8"
+                onClick={() =>
+                  counterCartItems(product.id) > 1
+                    ? decrementItem(product.id)
+                    : removeFromCart(product.id)
+                }
+              >
+                -
               </Button>
-            )}
-          </div>
-          <h3 className="font-bold text-xl">{product.title}</h3>
-
-          <div className="flex items-center gap-4 text-gray-500 text-sm mt-1">
-            <span>⚖ {product.weight}</span>
-            <span>Ø {product.size}</span>
-          </div>
-
-          <p className="text-gray-500 text-sm mt-2 line-clamp-2">
-            {product.ingredients}
-          </p>
+              <span className="text-lg font-semibold">
+                {counterCartItems(product.id)}
+              </span>
+              <Button
+                className="size-8"
+                onClick={() => incrementItem(product.id)}
+              >
+                +
+              </Button>
+            </div>
+          ) : (
+            <Button className="h-8" onClick={() => addToCart(product)}>
+              У кошик
+            </Button>
+          )}
         </div>
+        <h3 className="font-bold text-xl">{product.title}</h3>
+
+        <div className="flex items-center gap-4 text-gray-500 text-sm mt-1">
+          <span>⚖ {product.weight}</span>
+          <span>Ø {product.size}</span>
+        </div>
+
+        <p className="text-gray-500 text-sm mt-2 line-clamp-2">
+          {product.ingredients}
+        </p>
       </div>
-    </CartProvider>
+    </div>
   );
 }
 
